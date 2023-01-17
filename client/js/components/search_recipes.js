913b373368e5c96c6e3472136af855f256e1c1dd
clane <lane.caitlyn@gmail.com>
2023-01-17 12:43:25 +1100

    search results in progress

--- client/js/components/search_recipes.js
+++ client/js/components/search_recipes.js
@@ -21,15 +21,19 @@ function renderSearch() {
     fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchInput}&apiKey=247841a848f5433ba355a9d1cb38b293`)
       .then(response => response.json())
       .then(response => {
-        const searchOutput = response
+        const searchOutput = response.results
 
-        searchResults.innerHTML = `
-            <h2>${searchOutput.results[0].title}</h2>
-            <h2>${searchOutput.results[1].title}</h2>
-            <h2>${searchOutput.results[2].title}</h2>
-  
-          `
+        console.log(searchOutput)
+        displayRecipes(searchOutput)
+
       })
   }
 }
+
+// function displayRecipes(searchOutput) {
+//   return searchOutput.forEach(function (output) {
+//      `<section class="recipes">
+//       <h2>${output.title}</h2> 
+//       </section>`).join(" ")
+//     }
 
