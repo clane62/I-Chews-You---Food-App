3c9b3926207a217d89a6f802d116bd7f3111c216
gjstephen <gjstephen1@gmail.com>
2023-01-17 11:36:14 +1100

    update to user model and added unique constraint to users table

--- models/user.js
+++ models/user.js
@@ -2,6 +2,18 @@ const db = require('../db/db')
 
 const User = {
 
+    create: (first_name, last_name, username, email, passwordDigest) => {
+        const sql = `
+        INSERT INTO users(first_name, last_name, username, email, password_digest)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *
+        `
+
+        return db
+            .query(sql, [first_name, last_name, username, email, passwordDigest])
+            .then(dbRes => dbRes.rows[0].username)
+    },
+
     findById: id => {
         const sql = 'SELECT * FROM users WHERE id = $1'
 
