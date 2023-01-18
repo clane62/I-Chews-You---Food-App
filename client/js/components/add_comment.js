1d7334aca6c44f69cfae3df181b20d5107673261
0xJessez <jesse.zhang919@gmail.com>
2023-01-18 12:27:28 +1100

    comments functionality updated

--- client/js/components/add_comment.js
+++ client/js/components/add_comment.js
@@ -4,7 +4,7 @@ function renderAddComment() {
     // replace below line with relevant position comments box should go
     document.querySelector('#page').innerHTML = `
         <div class="comment-area">
-            <form>
+            <form onSubmit="addComment(event)">
                 <fieldset>
                     <label for="">Rating</label>
                     <input type="text" name="rating" value size="50" maxlength="50">
@@ -19,4 +19,19 @@ function renderAddComment() {
             </form>
         </div>
     `
+}
+
+function addComment(event) {
+    event.preventDefault()
+    const form = event.target
+    const data = Object.fromEntries(new FormData(form))
+
+    fetch('/api/comments', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+        .then(() => renderHome())
+
 }
