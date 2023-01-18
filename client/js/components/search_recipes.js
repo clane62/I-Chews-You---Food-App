438d9bc34d7612b4788e16258316b0f7de760b9e
clane <lane.caitlyn@gmail.com>
2023-01-18 13:59:52 +1100

    start of css

--- client/js/components/search_recipes.js
+++ client/js/components/search_recipes.js
@@ -1,55 +1,48 @@
 function renderSearch() {
-    document.querySelector('#page').innerHTML = `
+  document.querySelector('#page').innerHTML = `
   <form action="">
     <input type="text">
     <button class="search-recipe">Search</button>
   </form>
   <span class="search-results"></span>
   `
-    const searchResults = document.querySelector('.search-results')
-    const searchButton = document.querySelector('.search-recipe')
+  const searchResults = document.querySelector('.search-results')
+  const searchButton = document.querySelector('.search-recipe')
 
-    searchButton.addEventListener('click', event => {
-        event.preventDefault()
-        const searchInput = document.querySelector('input').value
-        console.log(searchInput)
+  searchButton.addEventListener('click', event => {
+    event.preventDefault()
+    const searchInput = document.querySelector('input').value
+    console.log(searchInput)
 
-        findRecipe(searchInput)
-    })
+    findRecipe(searchInput)
+  })
+
+  function findRecipe(searchInput) {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchInput}&apikey=9fb4e2597b3f405291312d55676dd441`)
+      .then(response => response.json())
+      .then(response => {
+        const searchOutput = response.results
+
+        console.log(searchOutput)
+        renderSearchList(searchOutput)
+
+      })
+  }
 }
 
-function findRecipe(searchInput) {
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchInput}&apiKey=9fb4e2597b3f405291312d55676dd441`)
-        .then(response => response.json())
-        .then(searchResults => {
-            const searchOutput = searchResults.results
-
-            console.log(searchOutput)
-            renderSearchList(searchOutput)
-
-        })
+function renderSearchList(searchOutput) {
+  document.querySelector('#page').innerHTML = `
+    <section class='search-rslt'>
+      ${renderSearchResults(searchOutput)}
+    </section>
+  `
 }
 
-function findRecipeById(recipeId) {
-  fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=9fb4e2597b3f405291312d55676dd441`)
-    .then(response => response.json())
-    .then(searchResult => {
-      console.log(searchResult)
-    })
+function renderSearchResults(searchOutput) {
+  return searchOutput.map(output => `
+   <section onClick='renderSingleRecipe(event)' class="recipe" data-id='${output.id}'>
+      <h2>${output.title}</h2>
+      <img src='${output.image}' alt=''>
+   </section>
+  `).join(" ")
 }
-// function renderSearchList(searchOutput) {
-//     document.querySelector('#page').innerHTML = `
-//     <section class='search-rslt'>
-//       ${renderSearchResults(searchOutput)}
-//     </section>
-//   `
-// }
-
-// function renderSearchResults(searchOutput) {
-//     return searchOutput.map(output => `
-//    <section onClick='renderSingleRecipe(event)' class="recipe" data-id='${output.id}'>
-//       <h2>${output.title}</h2>
-//       <img src='${output.image}' alt=''>
-//    </section>
-//   `).join(" ")
-// }
