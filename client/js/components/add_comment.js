db51e134f687886f240a065c6629f53e6fd4cda1
0xJessez <jesse.zhang919@gmail.com>
2023-01-18 11:55:32 +1100

    adding comments functionality

--- client/js/components/add_comment.js
+++ client/js/components/add_comment.js
@@ -1,10 +1,21 @@
 function renderAddComment() {
 
+
     // replace below line with relevant position comments box should go
     document.querySelector('#page').innerHTML = `
         <div class="comment-area">
             <form>
-                
+                <fieldset>
+                    <label for="">Rating</label>
+                    <input type="text" name="rating" value size="50" maxlength="50">
+                </fieldset>
+
+                <fieldset>
+                    <label for="">Comment</label>
+                    <textarea name="comment" id="" cols="50" rows="10"></textarea>
+                </fieldset>
+
+                <button>Add comment</button>
             </form>
         </div>
     `
