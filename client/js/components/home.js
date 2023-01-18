a6baacfa895a8093934a4ea9b6da5b807a2cf4ab
clane <lane.caitlyn@gmail.com>
2023-01-18 11:40:23 +1100

    feature post

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -1,5 +1,25 @@
 function renderHome() {
-    document.querySelector('#page').innerHTML = `
-        <h2>Welcome to I Chews You</h2>
+  document.querySelector('#page').innerHTML = `
+
     `
+  randomRecipe()
+
+}
+function randomRecipe() {
+
+  fetch(`https://api.spoonacular.com/recipes/random?apiKey=9fb4e2597b3f405291312d55676dd441 `)
+    .then(response => response.json())
+    .then(response => {
+      var food = response.recipes[0]
+
+      document.querySelector('#page').innerHTML += `
+      <header>
+        <h2>${food.title}</h2>
+        <button class="" onClick="renderHome()">Chews Another Recipe</button>
+        </header>
+        <img src='${food.image}' alt=''>
+`
+
+
+    })
 }
--- client/js/initialize.js
+++ client/js/initialize.js
@@ -17,3 +17,18 @@ const state = {
 //     .then(res => res.json())
 //     .then(userName => state.loggedInUserName = userName)
 
+fetch(`https://api.spoonacular.com/recipes/random?apiKey=9fb4e2597b3f405291312d55676dd441 `)
+  .then(response => response.json())
+  .then(response => {
+    var food = response.recipes[0]
+
+    document.querySelector('#page').innerHTML += `
+    <h2>Welcome to I Chews You</h2>
+    <header>
+      <h2>${food.title}</h2>
+      <button class="" onClick="renderHome()">Chews Another Recipe</button>
+      </header>
+      <img src='${food.image}' alt=''>
+
+`
+  })
