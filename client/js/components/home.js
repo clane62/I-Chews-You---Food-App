6ab17ac99dc6908a1dc5505e964bc0d09481b9c9
clane <lane.caitlyn@gmail.com>
2023-01-19 15:22:35 +1100

    home page, search page fixed

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -1,22 +1,26 @@
 function renderHome() {
-    // document.querySelector('#page').innerHTML = ``
-    randomRecipe()
+  // document.querySelector('#page').innerHTML = ``
+  randomRecipe()
 }
 
 function randomRecipe() {
 
-    fetch(`https://api.spoonacular.com/recipes/random?apiKey=99a56507b069468ea74c05caf5aac57b`)
-        .then(response => response.json())
-        .then(response => {
-            var food = response.recipes[0]
-            document.querySelector('#page').innerHTML = `
-      <button class="random-button" onClick="randomRecipe()">Chews Another Recipe</button>
-
-      <section onclick='renderSingleRecipe(event)' class='feature-recipe recipe' data-id='${food.id}'>
-        <div class="heading">
-          <h2>${food.title}</h2>
-        <div>
-        <img src='${food.image}' alt=''>
+  fetch(`https://api.spoonacular.com/recipes/random?apiKey=247841a848f5433ba355a9d1cb38b293`)
+    .then(response => response.json())
+    .then(response => {
+      var food = response.recipes[0]
+      document.querySelector('#page').innerHTML = `
+      <section class="random-icon">
+      <button class="  random-button" onClick="randomRecipe()">
+      Chews Another Recipe</button>
       </section>
-    `})
+      <div class="break"> </div>
+      <section onclick='renderSingleRecipe(event)' class='feature-recipe' data-id='${food.id}'>
+      <img src='${food.image}' alt=''>
+      <div class="heading">
+        <h2>${food.title}</h2>
+      </div>
+      </section>
+      `
+    })
 }
--- client/js/initialize.js
+++ client/js/initialize.js
@@ -1,7 +1,7 @@
 const state = {
-    // populate with other key value pairs
-    reviews: [],
-    loggedInUserName: null
+  // populate with other key value pairs
+  reviews: [],
+  loggedInUserName: null
 }
 
 // PLACEHOLDER CODE
@@ -14,22 +14,25 @@ const state = {
 //     })
 
 fetch('/api/sessions')
-    .then(res => res.json())
-    .then(userName => state.loggedInUserName = userName)
+  .then(res => res.json())
+  .then(userName => state.loggedInUserName = userName)
 
-fetch(`https://api.spoonacular.com/recipes/random?apiKey=99a56507b069468ea74c05caf5aac57b`)
-    .then(response => response.json())
-    .then(response => {
-        var food = response.recipes[0]
+fetch(`https://api.spoonacular.com/recipes/random?apiKey=16157713a2bb48259ac28d6dce7b2976`)
+  .then(response => response.json())
+  .then(response => {
+    var food = response.recipes[0]
 
-        document.querySelector('#page').innerHTML = `
-    <button class="random-button" onClick="randomRecipe()">Chews Another Recipe</button>
-
-    <section onclick='renderSingleRecipe(event)' class='feature-recipe recipe' data-id='${food.id}'>
-      <div class="heading">
-        <h2>${food.title}</h2>
-      <div>
-      <img src='${food.image}' class="feature-image" alt=''>
+    document.querySelector('#page').innerHTML = `
+    <section class="random-icon">
+    <button class="  random-button" onClick="randomRecipe()">
+    Chews Another Recipe</button>
+    </section>
+    <div class="break"> </div>
+    <section onclick='renderSingleRecipe(event)' class='feature-recipe' data-id='${food.id}'>
+    <img src='${food.image}' alt=''>
+    <div class="heading">
+      <h2>${food.title}</h2>
+    </div>
     </section>
     `
-    })
+  })
